e7dbf5039c022145daa0824f585b6fdd7e058b52
Menglong Keo <menglongkeo07@gmail.com>
2025-07-25 23:04:25 +0700

    Second initialize

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ function showCard() {
   let age = document.getElementById("userAge").value;
   let email = document.getElementById("userEmail").value;
   let position = document.getElementById("userPosition").value;
+  let image = document.getElementById("userImage").value;
+  console.log("image", image);
   document.getElementById("card").innerHTML = `<div
             class="bg-white border border-gray-200 rounded-lg shadow-sm py-8"
           >
             <div class="flex flex-col gap-1 items-center">
               <img
                 class="w-24 h-24 mb-3 rounded-full shadow-lg"
-                src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
+                src=${image}
                 alt="Bonnie image"
               />
               <h5 class="mb-1 text-xl font-medium text-gray-900">${name}</h5>
